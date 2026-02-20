ace82ab6d68d5862e80b867db66699627843b433
AbhinavBdrKoirala <abhinav.bahadurkoirala.s24@icp.edu.np>
2026-02-20 16:08:51 +0545

    Fixing bugs related to backend connection.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+console.log("INDEX FILE LOADED");
+
 const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
-    
+
 const app = express();
 
 app.use(cors());
@@ -11,8 +13,19 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+app.get('/api/protected', (req, res) => {
+    res.json({
+        message: "Protected route accessed successfully"
+    });
+});
+
 app.get('/', (req, res) => {
     res.send("RoyalMind Backend Running");
+});
+
+app.get('*', (req, res) => {
+    console.log("Request received for:", req.url);
+    res.status(404).json({ error: "Route not found in backend" });
 });
 
 app.listen(5000, () => {
